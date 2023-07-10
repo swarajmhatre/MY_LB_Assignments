535c6920831de50058bc44e3960293bce68c27e1
swarajmhatre <swarajmhatre25@gmail.com>
2023-07-10 15:44:20 +0530

    Assignment No. 41

---
+++ Assignements/program41_1.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Pattern {
+    public String str;
+
+    public Pattern(String str) {
+        this.str = str;
+    }
+
+    public void Display() {
+        char Arr[] = str.toCharArray();
+        System.out.println("The pattern is: ");
+        for (int i = 1; i < Arr.length; i++) {
+            for (int j = 0; j < Arr.length; j++) {
+                if (j < i) {
+                    System.out.print(Arr[j] + "\t");
+                } else {
+                    System.out.print("*\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+public class program41_1 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the String: ");
+        String str = sc.nextLine();
+
+        Pattern pt = new Pattern(str);
+
+        pt.Display();
+
+        sc.close();
+    }
+}
